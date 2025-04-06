12750ae10b1bc7660f9656c07aa45c6747ee637d
Shoons23 <jaehoon9723@gmail.com>
2025-04-06 23:40:37 +0900

    RuntimeError: input.size(-1) must be equal to input_size. Expected 128, got 1024

---
+++ Baseline.py
@@ -0,0 +1,109 @@
+# CNN 펼쳐보기
+
+import torch
+import torch.nn as nn
+
+
+class GLU(nn.Module):
+    def __init__(self, input_num):
+        super(GLU, self).__init__()
+        self.sigmoid = nn.Sigmoid()
+        self.linear = nn.Linear(input_num, input_num)
+
+    def forward(self, x):
+        lin = self.linear(x.permute(0, 2, 3, 1))
+        lin = lin.permute(0, 3, 1, 2)
+        sig = self.sigmoid(lin)
+        res = lin * sig
+        return res
+
+
+class ContextGating(nn.Module):
+    def __init__(self, input_num):
+        super(ContextGating, self).__init__()
+        self.sigmoid = nn.Sigmoid()
+        self.linear = nn.Linear(input_num, input_num)
+
+    def forward(self, x):
+        lin = self.linear(x.permute(0, 2, 3, 1))
+        lin = lin.permute(0, 3, 1, 2)
+        sig = self.sigmoid(lin)
+        res = x * sig
+        return res
+
+
+class CNN(nn.Module):
+    def __init__(
+        self,
+        n_in_channel,
+        activation="relu",
+        conv_dropout=0,
+        kernel_size=[3, 3, 3],
+        padding=[1, 1, 1],
+        stride=[1, 1, 1],
+        nb_filters=[64, 64, 64],
+        pooling=[(1, 4), (1, 4), (1, 4)],
+        normalization="batch",
+        **transformer_kwargs
+    ):
+
+        super(CNN, self).__init__()
+
+
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(n_in_channel, nb_filters[0], kernel_size[0], stride[0], padding[0]),
+            nn.BatchNorm2d(self.nb_filters[0]) if normalization == "batch" else nn.GroupNorm(1, nb_filters[0]),
+            self.get_activation_func(activation, nb_filters[0]),
+            nn.Dropout(conv_dropout),
+            nn.AvgPool2d(pooling[0])
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(nb_filters[0], nb_filters[1], kernel_size[1], stride[1], padding[1]),
+            nn.BatchNorm2d(nb_filters[1]) if normalization == "batch" else nn.GroupNorm(1, nb_filters[1]),
+            self.get_activation_func(activation, nb_filters[1]),
+            nn.Dropout(conv_dropout),
+            nn.AvgPool2d(pooling[1])
+
+        )        
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(nb_filters[1], nb_filters[2], kernel_size[2], stride[2], padding[2]),
+            nn.BatchNorm2d(nb_filters[2]) if normalization == "batch" else nn.GroupNorm(1, nb_filters[2]),
+            self.get_activation_func(activation, nb_filters[2]),
+            nn.Dropout(conv_dropout),
+            nn.AvgPool2d(pooling[2])
+        )
+            
+
+
+    def forward(self, x):
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        return x
+    
+    def get_activation_func(self, activ, nOut):
+        if activ.lower() == "leakyrelu":
+            return nn.LeakyReLU(0.2)
+        elif activ.lower() == "relu":
+            return nn.ReLU()
+        elif activ.lower() == "glu":
+            return GLU(nOut)
+        elif activ.lower() == "cg":
+            return ContextGating(nOut)
+
+
+
+if __name__ == "__main__":
+    model = CNN(n_in_channel=1)  # 입력 채널 1로 가정
+    x = torch.randn(1, 1, 128, 862)  # (batch_size, channel, height, width)
+
+    print("Input shape:", x.shape)
+
+    x = model(x)  
+
+    assert x.size(-1) == 128, f"GRU input mismatch: got {x.size(-1)}, expected 128"
